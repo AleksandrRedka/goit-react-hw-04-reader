68373c447660b4baf3d2da773f49d9a865cf382d
Aleksandr Redka <Aleksandr Redka>
2020-01-11 18:06:16 +0200

    Updates

---
+++ precache-manifest.6619d18b874329cfda20a3d8e00f3091.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f46af490d3d930f51af25a8e2f257041",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "6294492f540cfa08e5e8",
+    "url": "/goit-react-hw-04-reader/static/css/cast-list.8c6fefab.chunk.css"
+  },
+  {
+    "revision": "81b818e6e7b0caae0a0d",
+    "url": "/goit-react-hw-04-reader/static/css/home-page.eb85de71.chunk.css"
+  },
+  {
+    "revision": "3868af57edb4662cdde8",
+    "url": "/goit-react-hw-04-reader/static/css/main.edbdfcb6.chunk.css"
+  },
+  {
+    "revision": "c90e2566dcff473bfc4c",
+    "url": "/goit-react-hw-04-reader/static/css/movies-details-page.2da2aff9.chunk.css"
+  },
+  {
+    "revision": "1e650feb73c1db84a67c",
+    "url": "/goit-react-hw-04-reader/static/css/movies-page.c02d85b8.chunk.css"
+  },
+  {
+    "revision": "6e77b972cc57a7630841",
+    "url": "/goit-react-hw-04-reader/static/css/reviews-list.680bec9b.chunk.css"
+  },
+  {
+    "revision": "8e7472cf879f209fb9bd",
+    "url": "/goit-react-hw-04-reader/static/js/0.630ebb28.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/goit-react-hw-04-reader/static/js/0.630ebb28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfd4670dcc97e032c116",
+    "url": "/goit-react-hw-04-reader/static/js/8.cf3b8cb6.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/goit-react-hw-04-reader/static/js/8.cf3b8cb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6294492f540cfa08e5e8",
+    "url": "/goit-react-hw-04-reader/static/js/cast-list.0dd3d10d.chunk.js"
+  },
+  {
+    "revision": "81b818e6e7b0caae0a0d",
+    "url": "/goit-react-hw-04-reader/static/js/home-page.3a2d8c83.chunk.js"
+  },
+  {
+    "revision": "3868af57edb4662cdde8",
+    "url": "/goit-react-hw-04-reader/static/js/main.3a893792.chunk.js"
+  },
+  {
+    "revision": "c90e2566dcff473bfc4c",
+    "url": "/goit-react-hw-04-reader/static/js/movies-details-page.aa69d1e0.chunk.js"
+  },
+  {
+    "revision": "1e650feb73c1db84a67c",
+    "url": "/goit-react-hw-04-reader/static/js/movies-page.a4630ccc.chunk.js"
+  },
+  {
+    "revision": "6e77b972cc57a7630841",
+    "url": "/goit-react-hw-04-reader/static/js/reviews-list.31d47103.chunk.js"
+  },
+  {
+    "revision": "be3177340f7b0c2a6cd0",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.2698be2e.js"
+  }
+]);
